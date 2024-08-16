8b836be333d39941d1733e9c46608acbfcd3d117
Peter Fern <github@0xc0dedbad.com>
2024-08-16 17:22:08 +1000

    fix(compat): Fix Client.Fullscreen type for compat with Hyprland v0.42.0

--- internal/hypripc/client.go
+++ internal/hypripc/client.go
@@ -20,7 +20,7 @@ type Client struct {
 	Pid            int64         `json:"pid"`
 	Xwayland       bool          `json:"xwayland"`
 	Pinned         bool          `json:"pinned"`
-	Fullscreen     bool          `json:"fullscreen"`
+	Fullscreen     int           `json:"fullscreen"`
 	FullscreenMode int           `json:"fullscreenMode"`
 	FakeFullscreen bool          `json:"fakeFullscreen"`
 	Grouped        []interface{} `json:"grouped"`
